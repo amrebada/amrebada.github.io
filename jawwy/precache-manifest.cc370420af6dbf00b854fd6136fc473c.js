e18984ed05f98840aa7870355ea36f0dc3082f9e
amrebada <amr.app.engine@gmail.com>
2020-03-01 00:44:13 +0400

    Jawwy v1

---
+++ jawwy/precache-manifest.cc370420af6dbf00b854fd6136fc473c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded636a88b39435017582ea8aa494d61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6ad8f0f00bd26c9b381",
+    "url": "/static/css/main.492630ac.chunk.css"
+  },
+  {
+    "revision": "f70cf371a151379eb9af",
+    "url": "/static/js/2.0725ad33.chunk.js"
+  },
+  {
+    "revision": "bec1d98007c5b4f69a31a09a4da1dc97",
+    "url": "/static/js/2.0725ad33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ad8f0f00bd26c9b381",
+    "url": "/static/js/main.ebd67b1e.chunk.js"
+  },
+  {
+    "revision": "3fdb68a2297706f41434",
+    "url": "/static/js/runtime-main.b5ceec88.js"
+  },
+  {
+    "revision": "d1a7ce4c9ca24f5caea944471031c27f",
+    "url": "/static/media/ben1.d1a7ce4c.jpg"
+  },
+  {
+    "revision": "e71da6c32bdc6f7b9bb7022fa84915f8",
+    "url": "/static/media/ben1.e71da6c3.mp4"
+  },
+  {
+    "revision": "78e7453237a88e4208f795d28e37e0c5",
+    "url": "/static/media/doc1.78e74532.jpg"
+  },
+  {
+    "revision": "ff97aad3cb97bf2bb5f27fae621052a5",
+    "url": "/static/media/doc1.ff97aad3.mp4"
+  },
+  {
+    "revision": "3e81b9748fcca3c23f0b4da69139ab29",
+    "url": "/static/media/sha1.3e81b974.mp4"
+  },
+  {
+    "revision": "d777ceaa6b1edb9930a2a29f3479a235",
+    "url": "/static/media/sha1.d777ceaa.jpg"
+  }
+]);
